7dbef8b5a91750cd7cca64ee4c425261fd3d72c8
JamieDWilliams <147426454+JamieDWilliams@users.noreply.github.com>
2024-04-16 15:02:38 +0100

    Update CameraMovement.cs
    Created method topdownAngles, converts mainCam rotation to a topdown view instead of just doing it in-line. Additionally added comments

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -5,41 +5,62 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField] private Camera mainCam;
+    [SerializeField] private Transform target; // Position the cameras will orbit  
+
     [SerializeField] private Camera cornerCam;
-    [SerializeField] private Transform target;
 
     private Vector3 previousPosition;
 
     private void Start()
     {
-        mainCam.transform.position = target.position;
+        // Initial positions of both cameras, ten units behind the target
+        mainCam.transform.position = target.position; 
         mainCam.transform.Translate(new Vector3(0, 0, -10));
 
         cornerCam.transform.position = target.position;
         cornerCam.transform.Translate(new Vector3(0, 0, -10));
-        cornerCam.transform.eulerAngles = new Vector3(90, mainCam.transform.eulerAngles.y, mainCam.transform.eulerAngles.z);
+
+        // Initial rotation of the corner camera (birdseye view with same orientation as main camera)
+        cornerCam.transform.eulerAngles = topdownAngles();
     }
     // Update is called once per frame
     void Update()
     {
+        // Store the position of the mouse when right mouse is pressed
         if (Input.GetMouseButtonDown(1))
         {
             previousPosition = mainCam.ScreenToViewportPoint(Input.mousePosition);
         }
 
+        // While right mouse is still pressed update cameras' transforms based on the change of mouse position
+        // Main camera rotates in a similar fashion to you using your hand to spin a globe
         if (Input.GetMouseButton(1))
         {
+            // Direction the mouse has moved
             Vector3 direction = previousPosition - mainCam.ScreenToViewportPoint(Input.mousePosition);
 
+            
             mainCam.transform.position = target.position;
-
-            mainCam.transform.Rotate(new Vector3(1, 0, 0), direction.y * 180);
+            // Bounds how high and low the main camera can view
+            if (mainCam.transform.eulerAngles.x + direction.y * 180 > 5 && mainCam.transform.eulerAngles.x + direction.y * 180 < 85)
+            {
+                // Rotate around the local x axis by distance mouse moved in the y axis
+                mainCam.transform.Rotate(new Vector3(1, 0, 0), direction.y * 180);
+            }
+            // Rotate around the global y axis by distance mouse moved in the x axis
             mainCam.transform.Rotate(new Vector3(0, 1, 0), -direction.x * 180, Space.World);
             mainCam.transform.Translate(new Vector3(0, 0, -10));
 
-            cornerCam.transform.eulerAngles = new Vector3(90, mainCam.transform.eulerAngles.y, mainCam.transform.eulerAngles.z);
+            // Re-orient corner camera to match main cameras rotation from a topdown perspective
+            cornerCam.transform.eulerAngles = topdownAngles(); 
 
+            // Update previousPosition 
             previousPosition = mainCam.ScreenToViewportPoint(Input.mousePosition);
         }
+    }
+    // Returns the angles for a topdown view with same orientation as the main camera (birdseye view)
+    Vector3 topdownAngles()
+    {
+        return new Vector3(90, mainCam.transform.eulerAngles.y, mainCam.transform.eulerAngles.z);
     }
 }
